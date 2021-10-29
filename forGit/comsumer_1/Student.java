052086a1c18a8b5e699599c35f3e28872de895ff
mariiasirenko2 <79446249+mariiasirenko2@users.noreply.github.com>
2021-10-29 18:06:18 +0300

    Add files via upload

---
+++ forGit/comsumer_1/Student.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+public class Student {
+    String name;
+    String surname;
+
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+
+}
